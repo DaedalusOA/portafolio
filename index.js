f35f4c9919bab1adc04ec9e5720032e02a354178
Dedalus <148242414+DaedalusOA@users.noreply.github.com>
2025-03-30 10:15:35 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ document.getElementById('contactme').addEventListener('click', function () {
 
     // Copy the text to the clipboard
     navigator.clipboard.writeText("diegochoarteaga@gmail.com").then(() => {
-        alert('diegochoarteaga@gmail.com copied to clipboard!');
+        alert('email not available copied to clipboard!');
     }).catch(err => {
         console.error('Failed to copy text: ', err);
     });
